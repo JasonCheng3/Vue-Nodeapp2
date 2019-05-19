646857dbf0fbdd1120ee3173e3d2c3a7c49d2ed1
JasonCheng3 <asd61404@sesports.com.tw>
2019-05-19 18:41:39 +0800

    21 build element registerForm

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,43 +1,153 @@
 <template>
-    <div class="register">
-        <section class="form_container">
-            <div class="manage_tip">
-                <span class="title">祥倫在線後台管理系統</span>
-            </div>
-        </section>
-    </div>
+  <div class="register">
+    <section class="form_container">
+      <div class="manage_tip">
+        <span class="title">祥倫在線後台管理系統</span>
+        <!-- element ui start-->
+        <el-form :model="registerUser" status-icon :rules="rules" ref="registerForm" label-width="80px" class="registerForm">
+          <el-form-item label="用戶名" prop="name">
+            <el-input v-model="registerUser.name" autocomplete="off" placeholder="請輸入用戶名"></el-input>
+          </el-form-item>
+          <el-form-item label="郵箱" prop="email">
+            <el-input v-model="registerUser.email" autocomplete="off" placeholder="請輸入郵箱"></el-input>
+          </el-form-item>
+          <el-form-item label="密碼" prop="password">
+            <el-input v-model="registerUser.password" autocomplete="off" placeholder="請輸入密碼"></el-input>
+          </el-form-item>
+          <el-form-item label="確認密碼" prop="password2">
+            <el-input v-model="registerUser.password2" autocomplete="off" placeholder="請確認密碼"></el-input>
+          </el-form-item>
+          <el-form-item label="選擇身份">
+            <el-select v-model="registerUser.identity" placeholder="請選擇身份">
+              <el-option label="管理員" value="manager"></el-option>
+              <el-option label="員工" value="employee"></el-option>
+            </el-select>
+          </el-form-item>
+
+          <el-form-item>
+            <el-button type="primary" class="submit_btn" @click="submitForm('registerForm')">註冊</el-button>
+          </el-form-item>
+        </el-form>
+
+        <!-- element ui end -->
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "index",
-    components: {}
+  name: "register",
+  components: {},
+  data() {
+    var validatePass2 = (rule, value, callback) => {
+      if (value !== this.registerUser.password) {
+        callback(new Error("兩次輸入密碼不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      registerUser: {
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+        identity: ""
+      },
+      rules: {
+        name: [
+          {
+            required: true,
+            message: "用戶名不能為空",
+            trigger: 'blur'
+          },
+          {
+            min: 2,
+            max: 30,
+            message: "長度在2到30字符之間",
+            trigger: 'blur'
+          }
+        ],
+        email: [
+          {
+            type: "email",
+            required: true,
+            message: "郵箱格式不正確",
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "密碼不能為空",
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 30,
+            message: "長度在6到30之間",
+            trigger: 'blur'
+          }
+        ],
+        password2: [
+          {
+            required: true,
+            message: "確認密碼不能為空",
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 30,
+            message: "長度在6到30之間",
+            trigger: 'blur'
+          },
+          {
+            validator: validatePass2,
+            trigger: 'blur'
+          }
+        ]
+      }
+    };
+  }
 };
 </script>
 <style scoped>
 .register {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background:  url(../assets/bg.jpg) no-repeat center center;
-    background-size: 100% 100%;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: url(../assets/bg.jpg) no-repeat center center;
+  background-size: 100% 100%;
 }
 
 .form_container {
-    width: 370px;
-    height: 210px;
-    position: absolute;
-    top: 10%;
-    left: 34%;
-    padding: 25px;
-    border-radius: 5px;
-    text-align: center;
+  width: 370px;
+  height: 210px;
+  position: absolute;
+  top: 10%;
+  left: 34%;
+  padding: 25px;
+  border-radius: 5px;
+  text-align: center;
 }
 
 .form_container .manage_tip .title {
-    font-family: 'Microsoft YaHei';
-    font-weight: bold;
-    font-size: 26px;
-    color: #fff;
+  font-family: "Microsoft YaHei";
+  font-weight: bold;
+  font-size: 26px;
+  color: #fff;
+}
+
+.registerForm {
+  margin-top: 20px;
+  background-color: #fff;
+  padding: 20px 40px 20px 20px;
+  border-radius: 5px;
+  box-shadow: 0px 5px 10px #ccc;
+}
+
+.submit_btn {
+  width: 100%;
 }
 </style>
