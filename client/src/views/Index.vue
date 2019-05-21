d1e76aab002aa7a1bcb2c3f26d28448df4fb72ca
JasonCheng3 <asd61404@sesports.com.tw>
2019-05-21 19:40:58 +0800

    30 leftmenu  設置

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="index">
         <HeadNav></HeadNav>
+        <LeftMenu></LeftMenu>
         <router-view></router-view>
     </div>
 </template>
@@ -8,10 +9,12 @@
 <script>
 
 import HeadNav from '../components/HeadNav';
+import LeftMenu from '../components/LeftMenu';
 export default {
     name: "index",
     components: {
-        HeadNav
+        HeadNav,
+        LeftMenu
     }
 };
 </script>
